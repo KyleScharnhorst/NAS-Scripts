45cb09092af1e8dcbe3c4f9bd9593eb30cf4ff47
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-09-13 17:15:35 -0700

    Refactor: pushScripts changed to be more generic.
    Using for loops rather than copy pasting nearly identical commands.

--- pushScripts.sh
+++ pushScripts.sh
@@ -1,4 +1,22 @@
-cp /home/pi/git/NAS-Scripts/*.sh /home/pi
-sudo cp /home/pi/git/NAS-Scripts/*.sh /home/jeremy
-rm /home/pi/pushScripts.sh
-sudo rm /home/jeremy/pushScripts.sh
+#!/bin/bash
+
+##
+#Must be ran with root permissions. e.g., sudo ./pushScripts.sh
+##
+
+users=( pi jeremy alex )
+files=( mountNAS.sh updateNAS.sh searchNAS.sh )
+
+#Check for root privileges
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+for i in "${users[@]}"
+do
+    for j in "${files[@]}"
+    do
+        cp /home/pi/git/NAS-Scripts/$j /home/$i
+    done
+done
