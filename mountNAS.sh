f2e8f1573be279335eef02ae0f087fb70fcfca09
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-09-13 18:21:32 -0700

    Add: header with fstab and bash shebang.
    Provides info for automatically mounting NAS directories.

--- mountNAS.sh
+++ mountNAS.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+##
+# Script exists incase a manual mount needs to happen.
+# Can add the following snippet to your /etc/fstab to 
+# mount directories automatically:
+# //192.168.0.13/disk1_share /mnt/disk1_share cifs guest,_netdev 0 0
+# //192.168.0.13/disk2_share /mnt/disk2_share cifs guest,_netdev 0 0
+# //192.168.0.13/disk3_share /mnt/disk3_share cifs guest,_netdev 0 0
+# //192.168.0.13/disk4_share /mnt/disk4_share cifs guest,_netdev 0 0
+##
+
 sudo mount -t cifs //192.168.0.13/disk1_share /mnt/disk1_share -o rw
 sudo mount -t cifs //192.168.0.13/disk2_share /mnt/disk2_share -o rw
 sudo mount -t cifs //192.168.0.13/disk3_share /mnt/disk3_share -o rw
