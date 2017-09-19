f5c4f9e4376d18892d6fc2696669eb0a37e521ed
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-09-19 15:14:16 -0700

    Change: mount script and fstab info.
    needed to update for now required user login.

--- mountNAS.sh
+++ mountNAS.sh
@@ -4,13 +4,14 @@
 # Script exists incase a manual mount needs to happen.
 # Can add the following snippet to your /etc/fstab to 
 # mount directories automatically:
-# //192.168.0.13/disk1_share /mnt/disk1_share cifs guest,_netdev 0 0
-# //192.168.0.13/disk2_share /mnt/disk2_share cifs guest,_netdev 0 0
-# //192.168.0.13/disk3_share /mnt/disk3_share cifs guest,_netdev 0 0
-# //192.168.0.13/disk4_share /mnt/disk4_share cifs guest,_netdev 0 0
+#//192.168.0.13/disk1_share /mnt/disk1_share cifs _netdev,username=user,password=Bonita!956z 0 0
+#//192.168.0.13/disk2_share /mnt/disk2_share cifs _netdev,username=user,password=Bonita!956z 0 0
+#//192.168.0.13/disk3_share /mnt/disk3_share cifs _netdev,username=user,password=Bonita!956z 0 0
+#//192.168.0.13/disk4_share /mnt/disk4_share cifs _netdev,username=user,password=Bonita!956z 0 0
 ##
 
-sudo mount -t cifs //192.168.0.13/disk1_share /mnt/disk1_share -o rw
-sudo mount -t cifs //192.168.0.13/disk2_share /mnt/disk2_share -o rw
-sudo mount -t cifs //192.168.0.13/disk3_share /mnt/disk3_share -o rw
-sudo mount -t cifs //192.168.0.13/disk4_share /mnt/disk4_share -o rw
+for i in `seq 1 4`;
+do
+	DISK_DIR="disk${i}_share"
+	sudo mount -t cifs -o username=user,password=Bonita!956z //192.168.0.13/$DISK_DIR /mnt/$DISK_DIR
+done
