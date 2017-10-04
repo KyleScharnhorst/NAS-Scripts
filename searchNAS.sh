2902bd678e387711880c67135f7b149c6e77417f
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-10-03 23:35:44 -0700

    Fix: grep exit code wasn't handled.
    Web app would think there was an issue calling this script when no results were found.
    grep produces an exit code of 1 when there are no matching lines. This isn't an error for us.
    It just means that there were no results.

--- searchNAS.sh
+++ searchNAS.sh
@@ -8,3 +8,11 @@ done
 
 echo Searching for $ARGS
 locate -i -r "^/mnt/disk$ARGS" | grep -Eiv ".r[0-9]*$|.jpg$|.nfo$|.torrent$|.png$|.sfv$|sample|screens$|.srt$|proof$|.txt$|.jpeg$|.xml$|.db$|.dat$"
+
+#Grep exits with an exit code of 1 if there were no matching lines.
+#which means there are no results. Here we are going to handle that.
+if [ $? -eq 1 ]
+then
+	echo No Results Found.
+	exit 0
+fi
