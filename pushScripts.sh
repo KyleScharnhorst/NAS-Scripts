42affb3df3d121dc46367459a31131a80ad173bf
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-10-04 16:43:30 -0700

    Change: push scripts.
    Now pushes to bin and copies update to cron.daily.

--- pushScripts.sh
+++ pushScripts.sh
@@ -4,21 +4,26 @@
 #Must be ran with root permissions. e.g., sudo ./pushScripts.sh
 ##
 
-#users=( pi jeremy alex )
-#files=( mountNAS.sh updateNAS.sh searchNAS.sh )
+SCRIPT_LOC=/home/pi/git/NAS-Scripts
+UPDATE_SCRIPT=updateNAS.sh
+SEARCH_SCRIPT=searchNAS.sh
+BIN=/bin
+ETC=/etc
+CRON_DAILY=$ETC/cron.daily
+
+files=( $UPDATE_SCRIPT $SEARCH_SCRIPT )
 
 #Check for root privileges
-#if [ "$EUID" -ne 0 ]
-#  then echo "Please run as root"
-#  exit
-#fi
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-#for i in "${users[@]}"
-#do
-#    for j in "${files[@]}"
-#    do
-#        cp /home/pi/git/NAS-Scripts/$j /home/$i
-#    done
-#done
+#Copy files to bin
+for j in "${files[@]}"
+do
+  cp $SCRIPT_LOC/$j $BIN
+done
 
-cp /home/pi/git/NAS-Scripts/updateNAS.sh /etc/cron.daily
+#Make update happen once daily.
+cp $SCRIPT_LOC/$UPDATE_SCRIPT $CRON_DAILY
