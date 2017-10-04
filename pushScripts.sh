e552b9517b42de91d13bdb69d0132db76a2d86e0
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-10-03 23:34:31 -0700

    Change: push scripts.
    No longer needs to be pushed to users dirs. They interact with a web app now.
    However, we would like to push the update script to the cron.daily dir. So that
    the file db is updated daily.

--- pushScripts.sh
+++ pushScripts.sh
@@ -4,19 +4,21 @@
 #Must be ran with root permissions. e.g., sudo ./pushScripts.sh
 ##
 
-users=( pi jeremy alex )
-files=( mountNAS.sh updateNAS.sh searchNAS.sh )
+#users=( pi jeremy alex )
+#files=( mountNAS.sh updateNAS.sh searchNAS.sh )
 
 #Check for root privileges
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+#if [ "$EUID" -ne 0 ]
+#  then echo "Please run as root"
+#  exit
+#fi
 
-for i in "${users[@]}"
-do
-    for j in "${files[@]}"
-    do
-        cp /home/pi/git/NAS-Scripts/$j /home/$i
-    done
-done
+#for i in "${users[@]}"
+#do
+#    for j in "${files[@]}"
+#    do
+#        cp /home/pi/git/NAS-Scripts/$j /home/$i
+#    done
+#done
+
+cp /home/pi/git/NAS-Scripts/updateNAS.sh /etc/cron.daily
